099618e9914f404c18e4d3b50656dbd94a396285
Doom-Art <ouginak121@gmail.com>
2022-10-27 09:10:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileIO_assignment_parts_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
