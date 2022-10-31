95a2621168f7e92bdea78c6b7f7390ebd34f67d7
Doom-Art <ouginak121@gmail.com>
2022-10-31 09:33:08 -0400

    im wrong

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace FileIO_assignment_parts_1_3
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FileIO_assignment_parts_1_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (File.Exists(@"WordList.txt")){
+                foreach (string i in File.ReadLines(@"WordList.txt", Encoding.UTF8))
+                {
+                    Console.WriteLine(i);
+                }
+                int count = 0;
+                foreach (string i in File.ReadLines(@"WordList.txt", Encoding.UTF8))
+                {
+                    count++;
+                    foreach (char c in i)
+                    {
+                        if (c == ' ')
+                            count++;
+                    }
+                }
+                Console.WriteLine($"The text file has {count} words");
+            }
         }
     }
 }
