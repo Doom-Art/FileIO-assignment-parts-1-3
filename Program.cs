cfe186fd21c34d7ab6d79d1733fd78acc4ee0c6f
Doom-Art <ouginak121@gmail.com>
2022-11-01 09:38:38 -0400

    save

--- Program.cs
+++ Program.cs
@@ -7,12 +7,10 @@ namespace FileIO_assignment_parts_1_3
     {
         static void Main(string[] args)
         {
+            ///Part 1
             if (File.Exists(@"WordList.txt")){
-                foreach (string i in File.ReadLines(@"WordList.txt", Encoding.UTF8))
-                {
-                    Console.WriteLine(i);
-                }
                 int count = 0;
+                StreamWriter writer = new StreamWriter("CapitalWordList.txt");
                 foreach (string i in File.ReadLines(@"WordList.txt", Encoding.UTF8))
                 {
                     count++;
@@ -21,9 +19,50 @@ namespace FileIO_assignment_parts_1_3
                         if (c == ' ')
                             count++;
                     }
+                    writer.WriteLine(i.ToUpper());
                 }
                 Console.WriteLine($"The text file has {count} words");
+                writer.Close();
+                if (File.Exists(@"CapitalWordList.txt"))
+                    Console.WriteLine("The file has succesfully been written.");
+                else
+                    Console.WriteLine("The file does not exist.");
             }
+            Console.WriteLine();
+            ///Part 2 
+            if (File.Exists(@"PWordList.txt")){
+                int palindromeNum = 0;
+                int nonPalindromeNum = 0;
+                StreamWriter palinWriter = new StreamWriter("PalindromeList.txt");
+                StreamWriter nonPalinWriter = new StreamWriter("NonPalindromeList.txt");
+                foreach(string i in File.ReadLines(@"PWordList.txt", Encoding.UTF8))
+                {
+                    if (i == Reverse(i)){
+                        palindromeNum++;
+                        palinWriter.WriteLine(i);
+                    }
+                    else
+                    {
+                        nonPalindromeNum++;
+                        nonPalinWriter.WriteLine(i);
+                    }
+                }
+                Console.WriteLine($"There are {palindromeNum} palindromes in the file.");
+                Console.WriteLine($"There are {nonPalindromeNum} non palindromes in the file.");
+            }
+            Console.WriteLine();
+            ///Part 3
+
+
+        }
+        public static string Reverse(string stringToReverse)
+        {
+            string reversedVersion = "";
+            for (int i = (stringToReverse.Length) - 1; i >= 0; i--)
+            {
+                reversedVersion += stringToReverse[i];
+            }
+            return reversedVersion;
         }
     }
 }
